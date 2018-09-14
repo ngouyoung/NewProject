3111ae441f2f6563f2b49eb1cebe7ef1ef1fe49a
WADUHACK\Blang_Vibol_BV <vibolsmart@gmail.com>
2018-09-14 13:28:45 +0700

    - asdasd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,6 @@
                             {{ session('status') }}
                         </div>
                     @endif
-asdasd
                     You are logged in!
                 </div>
             </div>
